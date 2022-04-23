69c733d9df9c7f109b3422af52f392d16d5b3414
rchan321 <97616168+rchan321@users.noreply.github.com>
2022-04-23 14:48:27 -0400

    added form submit handler

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,22 +1,32 @@
+var userFormEl = document.querySelector("#user-form");
+var nameInputEl = document.querySelector("#username");
+
+var formSubmitHandler = function(event) {
+  event.preventDefault();
+  // get value from input element
+  var username = nameInputEl.value.trim();
+
+  if (username) {
+    getUserRepos(username);
+    nameInputEl.value = "";
+  } else {
+    alert("Please enter a Github username");
+  }
+  console.log(event);
+}
+
 var getUserRepos = function(user) {
     // format the github api url
     var apiUrl = "https://api.github.com/users/" + user + "/repos";
 
     // make a request to the url
-    fetch(apiUrl).then(function(reponse) {
-      reponse.json().then(function(data) {
+    fetch(apiUrl).then(function(response) {
+      response.json().then(function(data) {
         console.log(data);
       });
     });
   };
 
-getUserRepos();
+userFormEl.addEventListener("submit", formSubmitHandler);
 
-fetch("https://api.github.com/users/octocat/repos").then(function(response) {
-  response.json().then(function(data){
-    console.log(data);
-  });
-});
-
-console.log("outside");
 
